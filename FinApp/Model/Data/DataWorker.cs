2237ca961b05d3849494712243af0843c0cfff50
Максим Леонтьев <max.leontiev@mail.ru>
2023-03-27 18:19:53 +0500

    Метод добавления пользователя

--- FinApp/Model/Data/DataWorker.cs
+++ FinApp/Model/Data/DataWorker.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FinApp.Model.Data
 {
     public static class DataWorker
     {
-
+        public static string CreatUser(string login, string password)
+        {
+            string result = "Такой пользователь уже существует";
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                bool checkIsExist = db.Users.Any(el => (el.Login == login && el.Password == password));
+                if (!checkIsExist)
+                {
+                    User user = new User { Login = login, Password = password };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    result = "Сделано";
+                }
+            }
+            return result;
+        }
     }
 }
